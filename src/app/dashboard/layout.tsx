3d35b293c9b79fa972f3366377e1f55d4813a609
pashunt13 <pavelkozencev2002@gmail.com>
2024-12-30 11:43:28 +0300

    добавил верхнюю панель с тогглом темы и выходом из аккаунта

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,13 +2,29 @@ import { ReactNode } from 'react'
 import Image from 'next/image'
 import Logo from '@/../public/logo.png'
 import Link from 'next/link'
-import { DashboardLinks } from '../components/DashboardLinks'
+import { DashboardLinks } from '../components/dashboard/DashboardLinks'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { Button } from '@/components/ui/button'
+import { ThemeToggle } from '../components/theme/ThemeToggle'
+import { Menu } from 'lucide-react'
+import { signOut } from '@/lib/auth'
+import requireUser from '@/lib/hooks'
 
 interface IDashboardLayoutProps {
   children?: ReactNode
 }
 
-function DashboardLayout({ children }: IDashboardLayoutProps) {
+async function DashboardLayout({ children }: IDashboardLayoutProps) {
+  const session = await requireUser()
+
   return (
     <>
       <div className="min-h-screen w-full grid md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
@@ -29,6 +45,59 @@ function DashboardLayout({ children }: IDashboardLayoutProps) {
               </nav>
             </div>
           </div>
+        </div>
+        <div className="flex flex-col">
+          <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+                  <Menu className="h-5 w-5" />
+                  <span className="sr-only">Toggle navigation menu</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="left" className="flex flex-col">
+                <nav className="grid gap-2 mt-10">
+                  <DashboardLinks />
+                </nav>
+              </SheetContent>
+            </Sheet>
+
+            <div className="ml-auto flex items-center gap-x-4">
+              <ThemeToggle />
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="secondary" size="icon" className="rounded-full">
+                    <Image
+                      src={session.user?.image as string}
+                      alt="Profile"
+                      width={20}
+                      height={20}
+                      className="w-full h-full rounded-full"
+                    />
+                    <span className="sr-only">Toggle user menu</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem asChild>
+                    <Link href="/dashboard/settings">Settings</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <form
+                      className="w-full"
+                      action={async () => {
+                        'use server'
+                        await signOut()
+                      }}>
+                      <button className="w-full text-left">Log out</button>
+                    </form>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </header>
+          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">{children}</main>
         </div>
       </div>
     </>
